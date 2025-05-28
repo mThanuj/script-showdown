531c1ee195114dbf6ac8d07f7e1bca33eb1173f0
mthanuj <mullagurithanuj0@gmail.com>
2025-05-28 08:39:57 +0530

    add redisinsight for development

--- backend/src/lib/utils.ts
+++ backend/src/lib/utils.ts
@@ -1,7 +1,14 @@
+import redis from '../config/redis.config';
+
 export function base64Encode(str: string): string {
   return Buffer.from(str).toString('base64');
 }
 
 export function base64Decode(str: string): string {
   return Buffer.from(str, 'base64').toString('utf-8');
+}
+
+export async function setUser(id: string, name: string, userId: string) {
+  const userData = { name, userId };
+  await redis.set(`id:${id}`, JSON.stringify(userData));
 }
